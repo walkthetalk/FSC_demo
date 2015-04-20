8b9e0b408ed15c10b54483a92ac48b9ca882d654
Yi Qingliang <niqingliang2003@gmail.com>
2015-04-20 18:17:57 +0000

    parameter detail
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- app/src/main/java/com/example/ll/fsc_demo/parameterfile/ParameterFileDetailFragment.java
+++ app/src/main/java/com/example/ll/fsc_demo/parameterfile/ParameterFileDetailFragment.java
@@ -1,7 +1,17 @@
 package com.example.ll.fsc_demo.parameterfile;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +21,9 @@ import com.example.ll.fsc_demo.R;
 import com.example.ll.fsc_demo.database.FsContentProvider;
 import com.example.ll.fsc_demo.database.FsParamTbl;
 import com.example.ll.fsc_demo.dummy.DummyContent;
+
+import java.util.HashMap;
+import java.util.Hashtable;
 
 /**
  * A fragment representing a single Parameter File detail screen.
@@ -28,7 +41,10 @@ public class ParameterFileDetailFragment extends PreferenceFragment {
     /**
      * The dummy content this fragment is presenting.
      */
+    private Uri mUri;
     private DummyContent.DummyItem mItem;
+    private HashMap<String, String> mOldContent = new HashMap<>();
+    private HashMap<String, String> mNewContent = new HashMap<>();
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -42,18 +58,7 @@ public class ParameterFileDetailFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            /*
-            fillData(FsContentProvider.URI_PATH_FSPARAMS + "/" + String.valueOf(getArguments().getLong(ARG_ITEM_ID)),
-                    FsParamTbl.ABSTRACT,
-                    R.layout.fragment_fs_parameterfile_list, new int[] {
-                            R.id.fs_parameterfile_number,
-                            R.id.fs_parameterfile_name,
-                            R.id.fs_parameterfile_mode,
-                            R.id.fs_parameterfile_fiber_type,
-                    },
-                    R.id.checkableChild,
-                    R.id.fs_parameterfile_icon);
-                    */
+            mUri = Uri.parse(FsContentProvider.URI_PATH_FSPARAMS + "/" + String.valueOf(getArguments().getLong(ARG_ITEM_ID)));
         }
     }
 
@@ -64,6 +69,18 @@ public class ParameterFileDetailFragment extends PreferenceFragment {
         //View rootView = inflater.inflate(R.xml.prefs_fs_parameterfile, container, false);
 
         addPreferencesFromResource(R.xml.prefs_fs_parameterfile);
+        // disable persistent
+        int cnt = getPreferenceScreen().getPreferenceCount();
+        for (int i = 0; i < cnt; ++i) {
+            getPreferenceScreen().getPreference(i).setPersistent(false);
+        }
+
+        getPreferenceScreen().setEnabled(false);
+
+        if (mUri != null) {
+            fillData(mUri, FsParamTbl.ABSTRACT);
+        }
+
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             // TODO ((TextView) rootView.findViewById(R.id.parameterfile_detail)).setText(mItem.content);
@@ -75,36 +92,50 @@ public class ParameterFileDetailFragment extends PreferenceFragment {
     /**
      * fill data using simple cursor adapter
      */
-    private void fillData(final String uri, final String[] from, int layout, int[] to, int checker, int icon) {
-        /*
+    private void fillData(final Uri uri, final String[] from) {
         getLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<Cursor>() {
             @Override
             public Loader<Cursor> onCreateLoader(int var1, Bundle var2) {
                 CursorLoader cursorLoader = new CursorLoader(getActivity(),
-                        Uri.parse(uri),
+                        uri,
                         from, null, null, null);
                 return cursorLoader;
             }
 
             @Override
             public void onLoadFinished(Loader<Cursor> var1, Cursor var2) {
-                mAdapter.swapCursor(var2);
+                if (var2 == null || var2.getCount() != 1) {
+                    // TODO no any content
+                    return;
+                }
 
-                setListShown(true);
+                var2.moveToFirst();
+
+                for (int i = 1; i < var2.getColumnCount(); ++i) {
+                    String tmp = var2.getColumnName(i);
+                    Preference pref = getPreferenceScreen().findPreference("fsp_" + tmp);
+                    switch (var2.getType(i)) {
+                        case Cursor.FIELD_TYPE_STRING:
+                            if (pref instanceof EditTextPreference) {
+                                ((EditTextPreference) pref).setText(var2.getString(i));
+                            }
+                            break;
+                    }
+                }
+
+                var2.close();
+
+                getPreferenceScreen().setEnabled(true);
+                //mAdapter.swapCursor(var2);
+
+                //setListShown(true);
             }
 
             @Override
             public void onLoaderReset(Loader<Cursor> var1) {
-                mAdapter.swapCursor(null);
+                //mAdapter.swapCursor(null);
             }
         });
-        mAdapter = new ParameterFileListAdapter(getActivity(),
-                layout,
-                null, from, to, 0,
-                checker, icon, R.drawable.ic_activated, R.drawable.ic_blank,
-                10);
 
-        setListAdapter(mAdapter);
-        */
     }
 }
